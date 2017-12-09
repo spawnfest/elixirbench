e5622a60754ecaf4a2175541ac675d85332646af
Michał Muskała <michal@muskala.eu>
2017-12-09 08:27:23 +0100

    Add formatter config

---
+++ api/.formatter.exs
@@ -0,0 +1,13 @@
+# Used by "mix format"
+[
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    plug: 1,
+    plug: 2,
+    pipeline: 2,
+    scope: 2,
+    pipe_through: 1,
+    forward: 3,
+    socket: 2
+  ]
+]
--- api/lib/elixir_bench_web/endpoint.ex
+++ api/lib/elixir_bench_web/endpoint.ex
@@ -8,7 +8,9 @@ defmodule ElixirBenchWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :elixir_bench, gzip: false,
+    at: "/",
+    from: :elixir_bench,
+    gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
