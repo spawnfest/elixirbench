184a219490839760628c193edaff3b0cb70a299d
Alexey Bondarenko <alexeybondarenko@me.com>
2017-12-09 21:20:50 +0200

    Install ApolloClient

--- web/src/index.js
+++ web/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createHashHistory } from 'history'
+import App from 'containers/App'
 
 import registerServiceWorker from './registerServiceWorker'
 import createStore from './services/store'
-import App from './App'
+import createApolloClient from './services/apolloClient'
 
 const history = createHashHistory()
 const store = createStore({ history })
+const apolloClient = createApolloClient()
 
 ReactDOM.render((
   <App
     store={ store }
     history={ history }
+    apolloClient={ apolloClient }
   />
 ), document.getElementById('root'));
 
---
+++ web/src/services/apolloClient.js
@@ -0,0 +1,16 @@
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import { API_URL } from 'configs'
+
+export default () => new ApolloClient({
+  // By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
+  // to a different host
+  link: new HttpLink({
+    url: `${API_URL}/graphql`,
+  }),
+  cache: new InMemoryCache()
+});
