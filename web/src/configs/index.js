dbc7374bbec8a59d1e671260be26ec693554a652
Alexey Bondarenko <alexeybondarenko@me.com>
2017-12-09 21:20:50 +0200

    Install ApolloClient

---
+++ web/src/configs/index.js
@@ -0,0 +1,2 @@
+
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/graphql'
---
+++ web/src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Provider  } from 'react-redux'
+import { ApolloProvider } from 'react-apollo'
+
+import Routes from 'routes'
+
+const App = ({ apolloClient,store, history }) => (
+  <ApolloProvider client={ apolloClient }>
+    <Provider store={ store }>
+      <Routes history={ history } store={ store }/>
+    </Provider>
+  </ApolloProvider>
+)
+
+export default App
