2e60c0ca4a101cabded4da895ca85439adac345d
Andrew Dryga <andrew@dryga.com>
2017-12-09 23:33:29 +0200

    Bump commit hash in tests to make it live

--- runner/test/runner/job_test.exs
+++ runner/test/runner/job_test.exs
@@ -5,7 +5,7 @@ defmodule ElixirBench.Runner.JobTest do
   @tag timeout: :infinity
   describe "start_job/4" do
     test "starts runner with dependencies" do
-      IO.inspect start_job("priv/bench/results", "elixir-ecto/ecto", "mm/benches", "4bcaf9131aefb7f1e956ff196307e82f8f7a1870")
+      IO.inspect start_job("priv/bench/results", "elixir-ecto/ecto", "mm/benches", "c5647e87f445b05ba67422a46233599569e09176")
     end
   end
 
