9b910f7b7f3ee97211cde21e23e30b9356f47476
Alexey Bondarenko <alexeybondarenko@me.com>
2017-12-10 19:47:34 +0200

    Logo and deploy envs

---
+++ web/src/components/ElixirBenchLogo/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+
+import React from 'react';
+import SvgIcon from 'material-ui/SvgIcon';
+
+function ElixirBench(props) {
+  return (
+    <SvgIcon {...props}>
+      <g>
+        <g>
+    			<path d="M9.1,13.5L4,18.6c1.4,3.2,4.6,5.4,8.3,5.4c5,0,9.1-4.1,9.1-9.1c0-1.3-0.6-2.4-1.6-3.5l-6.3,6.3L9.1,13.5z"/>
+    		</g>
+    		<g>
+    			<path d="M9.1,10.7l4.3,4.3l4.9-4.9C15.4,7.3,11.6,4.5,13-0.4C8.1,2.7,3.2,9.7,3.2,14.9c0,0.5,0,1,0.1,1.5L9.1,10.7z"
+    				/>
+    		</g>
+    	</g>
+    </SvgIcon>
+  );
+}
+
+ElixirBench.muiName = 'SvgIcon';
+
+export default ElixirBench;
