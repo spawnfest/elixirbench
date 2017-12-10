b26a21110d1b2eb0726c22079c830f3968e22f1d
Andrew Dryga <andrew@dryga.com>
2017-12-11 00:26:17 +0200

    Unify Elixir formatter opts

--- runner/.formatter.exs
+++ runner/.formatter.exs
@@ -1,5 +1,17 @@
 # Used by "mix format"
 [
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    plug: 1,
+    plug: 2,
+    pipeline: 2,
+    scope: 2,
+    pipe_through: 1,
+    forward: 3,
+    socket: 2,
+    object: 2,
+    field: 2,
+    field: 3
+  ],
   line_length: 120
 ]
