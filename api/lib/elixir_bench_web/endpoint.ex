f0b49705ed2596645570b19eeb01a6957b15dc32
Michał Muskała <michal@muskala.eu>
2017-12-10 20:53:12 +0100

    Remove socket from endpoint

--- api/lib/elixir_bench_web/endpoint.ex
+++ api/lib/elixir_bench_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule ElixirBenchWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :elixir_bench
 
-  socket "/socket", ElixirBenchWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
