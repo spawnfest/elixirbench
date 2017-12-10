5736d76a8270db769dd56cb401aa81fe4e1eff6c
Michał Muskała <michal@muskala.eu>
2017-12-10 22:49:01 +0100

    create uuid extension

--- api/priv/repo/migrations/20171210214237_add_uuid_to_job.exs
+++ api/priv/repo/migrations/20171210214237_add_uuid_to_job.exs
@@ -2,6 +2,10 @@ defmodule ElixirBench.Repo.Migrations.AddUuidToJob do
   use Ecto.Migration
 
   def change do
+    execute """
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    """, "" # nothing on down
+
     alter table(:jobs) do
       add :uuid, :uuid, null: false, default: fragment("uuid_generate_v4()")
     end
