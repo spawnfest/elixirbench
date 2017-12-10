5c1e4c0f76954e9f7e1c3a81675ccda7e1d90c6a
Alexey Bondarenko <alexeybondarenko@me.com>
2017-12-10 17:12:55 +0200

    Add back links

--- web/src/components/Page/index.js
+++ web/src/components/Page/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { compose, pure } from 'recompose'
+import { Link } from 'react-router'
 import { withStyles } from 'material-ui/styles'
+
 import Typography from 'material-ui/Typography'
+import KeyboardArrowLeft from 'material-ui-icons/KeyboardArrowLeft'
 
 import Grid from 'components/Grid'
 
 import styles from './styles'
 
-const Page = ({ classes, title, children }) => (
+const Page = ({ classes, title, backLink, backTitle, children }) => (
   <div className={ classes.root }>
     <Grid container direction="column" wrap="nowrap">
+      { backLink && (
+        <Grid item>
+          <Link to={ backLink }>
+            <Grid container direction="row" wrap="nowrap" alignItems="center">
+              <Grid item>
+                <KeyboardArrowLeft />
+              </Grid>
+              <Grid item>
+                <Typography type="body1">{ backTitle }</Typography>
+              </Grid>
+            </Grid>
+          </Link>
+        </Grid>
+      )}
       <Grid item>
-        <Typography type="headline">{ title }</Typography>
+        <Grid container direction="row" wrap="nowrap" alignItems="center">
+          <Grid item>
+            <Typography type="subheading">{ title }</Typography>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item>
         { children }
